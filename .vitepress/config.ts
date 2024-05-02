2fd3e3eb9a97ab6d92ed8780acf243e405f0cdf1
imsfc <imsfc9@gmail.com>
2024-05-02 17:05:08 +0800

    设置cleanUrls

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   title: '幸福工厂工具箱',
   description: '生产规划、游戏资料查询。专为提升《幸福工厂》游戏体验而设计。',
   srcDir: 'src',
+  cleanUrls: true,
   lastUpdated: false,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
