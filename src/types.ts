35e0c9a576983bfb4e7f16b5574162bdbecbb5eb
imsfc <imsfc9@gmail.com>
2024-09-19 06:49:26 +0800

    补充类型文件

---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export type Id = string
+
+export interface AssemblyLine {
+  id: Id
+  outputItemId: Id | null
+  outputsPerMinute: number | null
+  recipeId: Id | null
+}
+
+export interface ModuleFactory {
+  id: Id
+  name: string
+  remark: string
+  data: AssemblyLine[]
+}
+
+export interface Item {
+  id: Id
+  name: string
+}
+
+export interface ItemAmount {
+  itemId: Id
+  amount: number
+}
+
+export interface Recipe {
+  id: Id
+  name: string
+  input: ItemAmount[]
+  output: ItemAmount[]
+  producedIn: Id
+  duration: number
+}
+
+export interface Building {
+  id: Id
+  name: string
+  powerUsage: number
+}
