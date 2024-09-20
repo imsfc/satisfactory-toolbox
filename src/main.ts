ad4a6dbeafbd473bdb671c49da4cbde09d865c41
imsfc <imsfc9@gmail.com>
2024-09-20 21:50:40 +0800

    添加依赖 pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
-import App from '@/App'
-import router from '@/router'
+import { createPinia } from 'pinia'
+
+import App from './App'
+import router from './router'
 
 import 'virtual:uno.css'
 
 const app = createApp(App)
 
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
