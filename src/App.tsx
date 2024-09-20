9898fbfc060013ed5a5697b5ca502463ff264b8a
imsfc <imsfc9@gmail.com>
2024-09-20 22:43:50 +0800

    增加暗色支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import { RouterView } from 'vue-router'
-import { NConfigProvider, NMessageProvider } from 'naive-ui'
-import { zhCN, dateZhCN } from 'naive-ui'
+import { useColorMode } from '@vueuse/core'
+import {
+  darkTheme,
+  dateZhCN,
+  NConfigProvider,
+  NMessageProvider,
+  zhCN,
+} from 'naive-ui'
+
+const colorMode = useColorMode()
+
+const isDark = computed(() => {
+  return (
+    (colorMode.store.value === 'auto'
+      ? colorMode.system.value
+      : colorMode.store.value) === 'dark'
+  )
+})
 
 export default defineComponent({
   setup() {
     return () => (
-      <NConfigProvider locale={zhCN} dateLocale={dateZhCN}>
+      <NConfigProvider
+        theme={isDark.value ? darkTheme : undefined}
+        locale={zhCN}
+        dateLocale={dateZhCN}
+      >
         <NMessageProvider>
           <RouterView />
         </NMessageProvider>
