1d57724312e810e543a9a0ccf7e0dd48ee641715
imsfc <imsfc9@gmail.com>
2024-09-20 09:50:34 +0800

    添加依赖 vite-imagetools

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { imagetools } from 'vite-imagetools'
 
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), imagetools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
