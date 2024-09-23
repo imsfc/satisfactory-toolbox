e71128a1ba48ce2f5c52300e72ff41b94259c925
imsfc <imsfc9@gmail.com>
2024-09-24 01:15:39 +0800

    修复 decimalRound 函数

--- src/utils/decimalHelper.ts
+++ src/utils/decimalHelper.ts
@@ -5,5 +5,7 @@ export const decimalRound = (
   decimalPlaces: number,
   rounding: Decimal.Rounding = Decimal.ROUND_UP,
 ) => {
-  return new Decimal(decimal).toDP(decimalPlaces, rounding).toNumber()
+  return new Decimal(Decimal.isDecimal(decimal) ? decimal.toNumber() : decimal)
+    .toDP(decimalPlaces, rounding)
+    .toNumber()
 }
